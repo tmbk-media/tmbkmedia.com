aeb5c92014a8f4843b3d805c2dd6495f1182bcc4
Oliver Gassman <oll@olivergassman.net>
2023-11-12 16:30:21 +1100

    Remove custom mdx components

--- src/components/mdx-components/mdx-components.tsx
+++
@@ -1,5 +0,0 @@
-import type { MDXComponents } from 'mdx'
-
-export const mdxComponents: MDXComponents = {
-  h1: <h1 className="font-sans text-7xl" />
-}
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MDXProvider } from '@mdx-js/react'
 
-import { mdxComponents } from './components/mdx-components/mdx-components.tsx'
-
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MDXProvider components={mdxComponents}>
+    <MDXProvider>
       <App />
     </MDXProvider>
   </React.StrictMode>
