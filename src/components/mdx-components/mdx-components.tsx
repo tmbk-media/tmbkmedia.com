c60c9c1719379dda5b424f8beba9254f4f2e1bfc
Oliver Gassman <oll@olivergassman.net>
2023-11-12 16:26:29 +1100

    Add some styling and CI

--- src/components/mdx-components/mdx-components.tsx
+++ src/components/mdx-components/mdx-components.tsx
@@ -1,3 +1,5 @@
-export const mdxComponents = {
+import type { MDXComponents } from 'mdx'
 
+export const mdxComponents: MDXComponents = {
+  h1: <h1 className="font-sans text-7xl" />
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MDXProvider } from '@mdx-js/react'
 
+import { mdxComponents } from './components/mdx-components/mdx-components.tsx'
+
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MDXProvider>
+    <MDXProvider components={mdxComponents}>
       <App />
     </MDXProvider>
   </React.StrictMode>
